0dc3a2e3d5965cee5af49a9720f7f664400a2753
Ivan Reshetnyak <ivan.reshetnyak@gmail.com>
2020-12-23 16:09:58 +0300

    v0.1.2 - Move 'format::riff' implementation to .cpp

---
+++ src/format_riff.cpp
@@ -0,0 +1,43 @@
+/***************************************************************
+ * Copyright (C) 2020
+ *    Reshetnyak Ivan
+ ***************************************************************/
+
+#include "pch.h"
+
+#include "format_riff.h"
+
+namespace spectral {
+namespace format {
+
+
+void riff::subchunk::Write( FILE *File ) const {
+  fwrite(&ID, sizeof(ID), 1, File);
+  fwrite(&Size, sizeof(Size), 1, File);
+  fwrite(Data, Size / BitsInByte, 1, File);
+}
+
+
+void riff::chunk::Write( FILE *File ) const {
+  fwrite(&ID, sizeof(ID), 1, File);
+  fwrite(&Size, sizeof(Size), 1, File);
+  fwrite(&Format, sizeof(Format), 1, File);
+  for (const auto &Subchunk : Subchunks)
+    Subchunk->Write(File);
+}
+
+
+void riff::Read( const std::string &FileName ) {
+  // TODO
+}
+
+
+void riff::Write( const std::string &FileName ) {
+  FILE *File = fopen(FileName.c_str(), "wb");
+  Chunk->Write(File);
+  fclose(File);
+}
+
+
+} /* End of 'format' namespace */
+} /* End of 'spectral' namespace */
--- src/format_riff.h
+++ src/format_riff.h
@@ -25,11 +25,7 @@ public:
     int32_t ID, Size;
     void *Data;
 
-    virtual void Write( FILE *File ) const {
-      fwrite(&ID, sizeof(ID), 1, File);
-      fwrite(&Size, sizeof(Size), 1, File);
-      fwrite(Data, Size / BitsInByte, 1, File);
-    }
+    virtual void Write( FILE *File ) const;
   };
 
   class chunk {
@@ -37,28 +33,13 @@ public:
     int32_t ID, Size, Format;
     std::vector<std::shared_ptr<subchunk>> Subchunks;
 
-    virtual void Write( FILE *File ) const {
-      fwrite(&ID, sizeof(ID), 1, File);
-      fwrite(&Size, sizeof(Size), 1, File);
-      fwrite(&Format, sizeof(Format), 1, File);
-      for (const auto &Subchunk : Subchunks)
-        Subchunk->Write(File);
-    }
+    virtual void Write( FILE *File ) const;
   };
 
   riff() = default;
   virtual ~riff() = default;
-
-  virtual void Read( const std::string &FileName ) override {
-    // TODO
-  }
-
-
-  virtual void Write( const std::string &FileName ) override {
-    FILE *File = fopen(FileName.c_str(), "wb");
-    Chunk->Write(File);
-    fclose(File);
-  }
+  virtual void Read( const std::string &FileName ) override;
+  virtual void Write( const std::string &FileName ) override;
 
 protected:
   std::shared_ptr<chunk> Chunk;
